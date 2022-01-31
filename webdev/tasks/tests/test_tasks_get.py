85b221f4a186503a1c68cff9e13b1a78d7880c61
johnnybigoo <jotalico@icloud.com>
2022-01-31 16:23:15 -0500

    Video #3 ~> creating Tasks - 2

--- webdev/tasks/tests/test_tasks_get.py
+++ webdev/tasks/tests/test_tasks_get.py
@@ -1,6 +1,22 @@
+import pytest
 from django.urls import reverse
+from pytest_django.asserts import assertContains
 
 
-def test_status_code(client):
-    response = client.get(reverse('tasks:home'))
+@pytest.fixture
+def response(client):
+    resp = client.get(reverse('tasks:home'))
+    return resp
+
+
+def test_status_code(response):
     assert response.status_code == 200
+
+
+def test_form_present(response):
+    assertContains(response, '<form')
+
+
+def test_save_button_present(response):
+    assertContains(response, '<button type="submit"')
+
