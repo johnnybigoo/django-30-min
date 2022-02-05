4d98ffb84f38ed112d923750240f5b5ebf9135bd
johnnybigoo <jotalico@icloud.com>
2022-02-04 19:02:27 -0500

    Video #4 ~> Save to the DB -2

--- webdev/tasks/tests/test_tasks_post.py
+++ webdev/tasks/tests/test_tasks_post.py
@@ -12,10 +12,18 @@ def response(client, db):
 def test_task_existent_db(response):
     assert Task.objects.exists()
 
+def test_redirecting_after_saving(response):
+    assert response.status_code == 302
 
-def test_form_present(response):
-    assertContains(response, '<form')
+
+@pytest.fixture
+def response_invalid_data(client, db):
+    resp = client.post(reverse('tasks:home'), data={'name': ''})
+    return resp
 
 
-def test_save_button_present(response):
-    assertContains(response, '<button type="submit"')
+def test_task_not_existent_db(response_invalid_data):
+    assert not Task.objects.exists()
+
+def test_page_with_invalid_data(response_invalid_data):
+    assert response_invalid_data.status_code == 400
--- webdev/tasks/views.py
+++ webdev/tasks/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
 from webdev.tasks.forms import NewTaskForm
 
 
@@ -8,4 +10,8 @@ def home(request):
         form = NewTaskForm(request.POST)
         if form.is_valid():
             form.save()
+            return HttpResponseRedirect(reverse('tasks: home'))
+        else:
+           return render(request, 'tasks/home.html', {'form': form}, status=400)
+
     return render(request, 'tasks/home.html')
